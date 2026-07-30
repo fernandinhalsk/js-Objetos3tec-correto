f89ab6f006a676aeb3fde014f8189e318381550a
fernandinhalsk <fernanda.climaco@escola.pr.gov.br>
2026-07-30 11:21:01 -0300

    juntaLista

---
+++ ordenacao/ordenacao II/arrays.js
@@ -0,0 +1,51 @@
+const edFolha = [
+    {
+        titulo: "PHP"
+        preço: 15
+    },
+        {
+        titulo: "JavaScript"
+        preço: 25
+    },
+        {
+        titulo: "Java"
+        preço: 30
+    },
+        {
+        titulo: "Go"
+        preço: 45
+    },
+        {
+        titulo: "Elixir"
+        preço: 50
+    }
+]
+
+const edGalho = [
+    {
+        titulo: "Python"
+        preço: 20
+    },
+        {
+        titulo: "Rust"
+        preço: 22
+    },
+        {
+        titulo: "Ruby"
+        preço: 28
+    },
+        {
+        titulo: "C#"
+        preço: 33
+    },
+        {
+        titulo: "C++"
+        preço: 35
+    }
+        {
+        titulo: "Scala"
+        preço: 40
+    }
+]
+
+module.exports = {edFolha, edGalho}
